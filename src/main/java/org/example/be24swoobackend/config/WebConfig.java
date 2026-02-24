4d6fac6faecf113e210db7ba343bf4d0e962d287
swoo <kokodd1234@gmail.com>
2026-02-24 12:49:16 +0900

    [Refactor] 파일 구조 변경

--- src/main/java/org/example/be24swoobackend/config/WebConfig.java
+++ src/main/java/org/example/be24swoobackend/config/WebConfig.java
@@ -1,4 +1,4 @@
-package org.example.be24swoobackend.board.config;
+package org.example.be24swoobackend.config;
 
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
